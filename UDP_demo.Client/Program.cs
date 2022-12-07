d4f0830d030bdef6f6b20fdcb59dbf632a02abe6
Starinin Andrey <starininandrey@gmail.com>
2022-12-07 19:09:03 +0300

    Init

---
+++ UDP_demo.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+ 
+using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+ 
+Console.Write("Введите сообщение - ");
+var message = Console.ReadLine();
+var data = Encoding.UTF8.GetBytes(message);
+EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8005);
+var bytes = await udpSocket.SendToAsync(data, SocketFlags.None, remotePoint);
+Console.WriteLine($"Отправлено {bytes} байт");
---
+++ UDP_demo.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+ 
+var localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8005);
+udpSocket.Bind(localIP);
+Console.WriteLine("UDP-сервер запущен...");
+
+while (true)
+{
+    var data = new byte[256];
+    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+    var result = await udpSocket.ReceiveFromAsync(data, SocketFlags.None, remoteIp);
+    var message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
+ 
+    Console.WriteLine($"Получено {result.ReceivedBytes} байт");
+    Console.WriteLine($"Удаленный адрес: {result.RemoteEndPoint}");
+    Console.WriteLine(message);
+
+    if (message == "/STOP")
+    {
+        break;
+    }
+}
